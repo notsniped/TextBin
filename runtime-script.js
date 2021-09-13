2076bf0569d609101f2a7005d46e635e807271b9
SniperBlaze <72265661+shoddyDEV@users.noreply.github.com>
2021-09-13 12:35:33 +0530

    Update runtime-script.js

--- runtime-script.js
+++ runtime-script.js
@@ -1,3 +1,4 @@
+var darkModeEnabled = 0;
 function secureText() {
     console.log("Function Started");
     if (document.getElementById("Input-Secure-Text-Entry").value = true) {
@@ -5,5 +6,29 @@ function secureText() {
     }
     else if (document.getElementById("Input-Secure-Text-Entry").value = false) {
         document.getElementById("Text_Area-Input").type = "text";        
+    }
+}
+function switchMode() {
+    if (darkModeEnabled == 0) {
+        console.log(darkModeEnabled);
+        document.body.style.backgroundColor = "#121212";
+        document.body.style.color = "#FFFFFF";
+        document.getElementById("Text_Area-Input").style.backgroundColor = "#292929";
+        document.getElementById("Text_Area-Input").style.color = "#FFFFFF";
+        document.getElementById("Input-Change-Theme").innerHTML = "Switch to Light Mode";
+        darkModeEnabled = 1;
+        console.log("New Status:" + darkModeEnabled);
+        return
+    }
+    else if (darkModeEnabled == 1) {
+        console.log(darkModeEnabled);
+        document.body.style.backgroundColor = "#FFFFFF";
+        document.body.style.color = "#000000";
+        document.getElementById("Text_Area-Input").style.backgroundColor = "#FFFFFF";
+        document.getElementById("Text_Area-Input").style.color = "#000000";
+        document.getElementById("Input-Change-Theme").innerHTML = "Switch to Dark Mode";
+        darkModeEnabled = 0;
+        console.log("New Status:" + darkModeEnabled);
+        return
     }
 }
